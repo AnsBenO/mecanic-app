f55a5715ea3499afdaf3764c187d202278ffed70
AnsBenO <ans.benomar@gmail.com>
2024-03-18 01:06:01 +0000

    added routes for admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,4 +11,14 @@ Route::get('/', function () {
 
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+    // Routes for managing clients
+    Route::get('/clients', [ClientController::class, 'index'])->name('admin.clients.index');
+    Route::get('/clients/create', [ClientController::class, 'create'])->name('admin.clients.create');
+    Route::post('/clients', [ClientController::class, 'store'])->name('admin.clients.store');
+
+    // Routes for managing vehicles
+    Route::get('/vehicles', [VehicleController::class, 'index'])->name('admin.vehicles.index');
+    Route::get('/vehicles/create', [VehicleController::class, 'create'])->name('admin.vehicles.create');
+    Route::post('/vehicles', [VehicleController::class, 'store'])->name('admin.vehicles.store');
 });
